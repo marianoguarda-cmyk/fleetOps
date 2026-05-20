b71e3cf018aad61e3d56b03e51dd8c91c22ac3f0
marianoguarda-cmyk <mariano.guarda@chazki.com>
2026-05-20 17:40:38 -0300

    fix: serve operador.html correctly

--- app.js
+++ app.js
@@ -3,8 +3,19 @@ const path = require('path');
 
 const app = express();
 
+// Servir archivos estáticos desde la raíz
 app.use(express.static(path.join(__dirname)));
 
+// Ruta específica para el operador
+app.get('/operador', (req, res) => {
+  res.sendFile(path.join(__dirname, 'operador.html'));
+});
+
+app.get('/operador.html', (req, res) => {
+  res.sendFile(path.join(__dirname, 'operador.html'));
+});
+
+// Todo lo demás → index.html (panel admin)
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'index.html'));
 });
